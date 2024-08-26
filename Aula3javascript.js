cd0172c7f20dd1795bddc9d365e7796904770ae6
Marcelo Eloy <marcosfaro72@gmail.com>
2024-08-26 15:21:45 -0300

    Exercicío introdutório Javascript

---
+++ Aula3javascript.js
@@ -0,0 +1,18 @@
+let N1 = 10;
+let N2 = 10;
+const soma = N1 + N2;
+const subt = N1 - N2;
+const multi = N1 * N2;
+if(N2 != 0){
+    const divi = N1/N2;
+console.log("A soma dos dois números é: "+soma);
+console.log("A subtração dos dois números é: "+subt);
+console.log("A multiplicação dos dois números é: "+multi);
+console.log("A divisão dos dois números é: "+divi);
+}
+else{
+    console.log("A soma dos dois números é: "+soma);
+    console.log("A subtração dos dois números é: "+subt);
+    console.log("A multiplicação dos dois números é: "+multi);
+    console.log("Impossível dividir por 0!")
+}
